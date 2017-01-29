14a0c6bdc8d9cfb3ea1375f8c6314b1bc1d38b5a
StaphSynth <StaphSynth@users.noreply.github.com>
2017-01-30 00:15:18 +1100

    some basic functionality working

--- chproxy.rb
+++ chproxy.rb
@@ -3,24 +3,64 @@
 require 'nokogiri'
 require 'rest-client'
 require "highline/import"
+require 'pry'
 # require 'open-uri'
 
-proxyNova = "https://proxynova.com"
-page = Nokogiri::HTML(RestClient.get(proxyNova))
-rawLinks = page.css("li a")
-countryLinks = Array.new
-selection = Array.new
-for link in rawLinks
-  if link["href"].include? "country"
-    countryLinks.push link
-    selection.push link.text
+#go to proxynova.com and grab their list of countries with proxies
+def getCountries
+  proxyNova = "https://proxynova.com"
+  page = Nokogiri::HTML(RestClient.get(proxyNova))
+  rawLinks = page.css("li a")
+  countryLinks = Array.new
+  selection = Array.new
+  for link in rawLinks
+    if link["href"].include? "country"
+      # countryLinks.push link
+      selection.push [link.text, link["href"]]
+    end
   end
+  return #return some stuff, like a list of countries and whatnot would be nice I guess
+end #getCountries
+
+def getProxies(url)
+  eliteTable = []
+  page = Nokogiri::HTML(RestClient.get(url))
+  rawProxies = page.css('[id="tbl_proxy_list"] tbody tr')
+  # binding.pry
+  for row in rawProxies
+    #ignore the table row with the ads in it :)
+    if(!(row.css('td')[0].text.include? "adsbygoogle"))
+      #select only Elite level servers (highest level of anonymity)
+      if(row.css('td')[6].text.upcase.include? "ELITE")
+        tempRow = []
+        for i in 0..7
+          tempRow.push(row.css('td')[i].text.gsub(/\s+/, ""))
+        end
+        eliteTable.push tempRow
+      end
+    end
+    #print the table in a vaguely human-readable form for now
+    for row in eliteTable
+      string = ""
+      for element in row
+        string += element + ","
+      end
+      puts string
+    end
+
+  end
+end #getProxies
+
+#ask the user to select which country's proxies they want to use
+def menu
+  mm = HighLine.new
+  mm.choose do |menu|
+    menu.prompt = "Pick a country: "
+    menu.choices(*selection) do |chosen|
+      #code here
+    end #|chosen|
+  end #|menu|
 end
 
-mm = HighLine.new
-mm.choose do |menu|
-  menu.prompt = "Pick a country: "
-  menu.choices(*selection) do |chosen|
-    exit
-  end #|chosen|
-end #|menu|
+#testing...
+getProxies "https://www.proxynova.com/proxy-server-list/country-jp/"
