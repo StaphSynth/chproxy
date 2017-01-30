68a5349aba18df3fa1fd491f498c0c04a6eaed3a
StaphSynth <StaphSynth@users.noreply.github.com>
2017-01-30 15:27:02 +1100

    getProxy now performs rudimentary evaluation on available proxies and returns the 'best' one

--- chproxy.rb
+++ chproxy.rb
@@ -22,12 +22,13 @@ def getCountries
   return #return some stuff, like a list of countries and whatnot would be nice I guess
 end #getCountries
 
-def getProxies(url)
+#retrieves a a list of proxies from the provided url to proxynova
+#evaluates the 'best' one and returns an array containing its ip and port values
+def getProxy(url)
   eliteTable = []
   page = Nokogiri::HTML(RestClient.get(url))
   rawProxies = page.css('[id="tbl_proxy_list"] tbody tr')
-  # binding.pry
-  for row in rawProxies
+  rawProxies.each do |row|
     #ignore the table row with the ads in it :)
     if(!(row.css('td')[0].text.include? "adsbygoogle"))
       #select only Elite level servers (highest level of anonymity)
@@ -45,21 +46,29 @@ def getProxies(url)
         for i in 4..7
           tempRow.push(row.css('td')[i].text.gsub(/\s+/, ""))
         end
-        binding.pry
+        #re-format the up-time column into a useful integer
+        #(strips the % value from the string and re-casts as int)
+        tempRow[4] = (tempRow[4].split("%"))[0].to_i
+        #Row structure ["IP-ADDR","PORT",BOOL UP/DOWN,FLOAT 0-100(%) relative speed,INT 0-100(%) uptime,"COUNTRY","ELITE","YOUTUBE SCORE"]
         eliteTable.push tempRow
+      end #if
+    end #if
+  end #for
+  #evaluate the 'best' proxy
+  best=["",""] #["ip addr","port"]
+  topScore = 0
+  eliteTable.each do |row|
+    if(row[2]) #if it's up
+      #weight speed, then add it to uptime to produce score
+      score = ((row[3] * 1.3) + row[4])
+      # puts "#{row[0]} #{row[1]} #{score}"
+      if(score > topScore)
+        best[0] = row[0]
+        best[1] = row[1]
       end
     end
-    #  binding.pry
-    #print the table in a vaguely human-readable form for now
-    for row in eliteTable
-      string = ""
-      for element in row
-          string += element.to_s + ","
-      end
-      puts string
-    end
-
-  end
+  end #for
+  return best
 end #getProxies
 
 #ask the user to select which country's proxies they want to use
@@ -74,4 +83,12 @@ def menu
 end
 
 #testing...
-getProxies "https://www.proxynova.com/proxy-server-list/country-jp/"
+proxy = getProxy "https://www.proxynova.com/proxy-server-list/country-jp/"
+
+system "gsettings set org.gnome.system.proxy.http host '#{proxy[0]}'"
+system "gsettings set org.gnome.system.proxy.http port '#{proxy[1]}'"
+system "gsettings set org.gnome.system.proxy.https host '#{proxy[0]}'"
+system "gsettings set org.gnome.system.proxy.https port '#{proxy[1]}'"
+system "gsettings set org.gnome.system.proxy.socks host '#{proxy[0]}'"
+system "gsettings set org.gnome.system.proxy.socks port '#{proxy[1]}'"
+system "gsettings set org.gnome.system.proxy mode 'manual'"
